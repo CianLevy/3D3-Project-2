ab7d905206b1b62d96790bac19aaa3c6afa7acf5
ECheng1998 <49043550+ECheng1998@users.noreply.github.com>
2019-04-01 11:18:14 +0100

    Log

--- log.cpp
+++ log.cpp
@@ -1,12 +1,37 @@
 #include "log.hpp"
 
+
 logger::logger(char ID){
-
+	std::string route_ID = "routing-output" << ID;
 }
-void logger::recordTableUpdate(route_table rt){
 
+void logger::recordTableUpdate(route_table rt){
+	//Reading whats in the file first
+	fileStreamI.open("route_ID");
+	std::string line; //Current line in the file
+	getline(fileStreamI, line); //Loading the first line
+
+	if(fileStreamI.is_open()){
+		do{
+			std::cout << line << '\n';
+		  } while (getline(fileStreamI, line));
+	fileStreamI.close();
+	}
+
+	//Writing to the file
+	fileStreamO.open("route_ID");
+
+	time_t update_time;
+	update_time = time(NULL);
+	char *tm = ctime(&update_time);
+	fileStreamO << "Router updated at " << tm << std::endl;
+
+	if(fileStreamO.is_open()){
+		outputStream << rt << std::endl;
+	}
 }
 
 void logger::recordRoutedDatagram(datagram d, uint16_t arrivalPort, uint16_t departPort){
+
 
 }
