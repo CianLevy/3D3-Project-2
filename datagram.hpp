a45860259c6b05c20df993deba72debe1ddf535d
ECheng1998 <49043550+ECheng1998@users.noreply.github.com>
2019-04-03 11:47:39 +0100

    Added a function to get the destination ID
    I needed a function to get the destination ID of the packet and that wasn't available so I just added it

--- datagram.hpp
+++ datagram.hpp
@@ -7,10 +7,7 @@
     This class encapsulates the datagrams which are transmitted between routers. It is used by the router class to both construct and deconstruct datagrams.
     From the sender point of view the class will be used to multiplex the payload data and datagram header.
     From the receiver point of view the class will be used to demultiplex the payload data and header fields.
-
     Note: The header currently contains only the data type and source ID. Other fields will most likely need to be added.
-
-
 */
 
 
@@ -27,6 +24,7 @@ class datagram{
         std::vector<uint8_t> getPayload() { return payload; };
         char getType() { return t; };
         char getID() { return sourceID; };
+        char getDestID() { return destID; };
         uint8_t getLength() { return length; };
 
     private:
