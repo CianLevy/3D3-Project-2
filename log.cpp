c96e3553fc0899c3032239a0eb42c483f751e06e
ECheng1998 <49043550+ECheng1998@users.noreply.github.com>
2019-04-03 11:53:33 +0100

    Added functionalities to recordRoutedDatagram. Still has issues
    -> I think recordRoutedDatagram is complete but I'm not 100% sure. I don't know what "type t" is in the datagram class. From my understanding, it's just to get the type of a different variable so I don't think I need to record that.
    -> For recordRoutedDatagram, I'm recording all the logs into a txt file called "log_file.txt". Is this alright? Or should I store it somewhere else?
    -> Line 29 has issues, obviously. Will fix soon.

--- log.cpp
+++ log.cpp
@@ -2,36 +2,51 @@
 
 
 logger::logger(char ID){
-	std::string route_ID = "routing-output" << ID;
+	std::string tempID = "route_ID";
+	tempID += ID;
+	routeID = tempID;
 }
 
 void logger::recordTableUpdate(route_table rt){
-	//Reading whats in the file first
-	fileStreamI.open("route_ID");
 	std::string line; //Current line in the file
-	getline(fileStreamI, line); //Loading the first line
 
-	if(fileStreamI.is_open()){
+	fileStream.open(routeID); //Opening the specified file
+	getline(fileStream, line);
+	if(fileStream.is_open()){
 		do{
 			std::cout << line << '\n';
-		  } while (getline(fileStreamI, line));
-	fileStreamI.close();
+		} while(getline(fileStream, line));
+	fileStream.close();
 	}
+	previousTable = rt;
+	fileStream.open(routeID);//Or fileStreamO.open("route_ID", ios::app) so there's no overwriting.
 
-	//Writing to the file
-	fileStreamO.open("route_ID");
-
-	time_t update_time;
+	time_t update_time; //Time sampling
 	update_time = time(NULL);
 	char *tm = ctime(&update_time);
-	fileStreamO << "Router updated at " << tm << std::endl;
+	fileStream << "Router updated at " << tm << std::endl;
 
-	if(fileStreamO.is_open()){
-		outputStream << rt << std::endl;
-	}
+	fileStream << rt << std::endl; //Writing to the file. This line doesn't work
+	fileStream.close();
 }
 
 void logger::recordRoutedDatagram(datagram d, uint16_t arrivalPort, uint16_t departPort){
+	std::ofstream loggerFile;
+	std::vector<uint8_t> pay = d.getPayload();
+	uint8_t payLength = d.getLength();
+	char sourceID = d.getID();
+	char destID = d.getDestID();
+	loggerFile.open("log_file.txt", std::ios::app); //Not sure if we want a txt file to store all the logs or we want to cout or what
+	loggerFile << "The Packet Source ID is " << sourceID << std::endl;
+	loggerFile << "The Destination ID is " << destID << std::endl;
+	loggerFile << "Arrival UDP Port is " << unsigned(arrivalPort) << std::endl;
+	loggerFile << "The Forward Port is " << unsigned(departPort) << std::endl;
 
+	//Printing the entire payload
+	loggerFile << "The Packet Payload contains: " << std::endl;
+	for(int i = 0; i <= payLength; i++){
+		loggerFile << unsigned(pay[i]) << std::endl;
+	}
 
+	loggerFile.close();
 }
