0b2ac547dd34f013d38d7144603329134d8b4bf4
ECheng1998 <49043550+ECheng1998@users.noreply.github.com>
2019-04-05 12:56:42 +0100

    Update log.cpp

--- log.cpp
+++ log.cpp
@@ -32,6 +32,22 @@ void logger::recordTableUpdate(route_table rt){
 }
 
 void logger::recordRoutedDatagram(datagram d, uint16_t arrivalPort, uint16_t departPort){
+	std::vector<uint8_t> pay = d.getPayload();
+	uint8_t payLength = d.getLength(); /* Getting the size of the payload */
+	char sourceID = d.getID();
+	char destID = d.getDestID();
+	fileStream.open("log_file.txt", std::ios::app); /* Opening a seperate log file to store all transfers */
+	fileStream << "The Packet Source ID is " << sourceID << std::endl;
+	fileStream << "The Destination ID is " << destID << std::endl;
+	fileStream<< "Arrival UDP Port is " << unsigned(arrivalPort) << std::endl;
+	fileStream << "The Forward Port is " << unsigned(departPort) << std::endl;
 
+	/* Printing the entire payload */
+	fileStream << "The Packet Payload contains: " << std::endl;
+	for(int i = 0; i <= payLength; i++){
+		fileStream << unsigned(pay[i]) << std::endl;
+	}
+
+	fileStream.close();
 
 }
