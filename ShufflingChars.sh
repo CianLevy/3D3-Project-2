46d552b9200a15e7ce6c77524ac0704dadd94a62
ECheng1998 <49043550+ECheng1998@users.noreply.github.com>
2019-04-07 16:11:00 +0100

    Working bash script to 'create' random characters
    Array holding a set of randomly sorted characters going from A-F

--- ShufflingChars.sh
+++ ShufflingChars.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-declare -a characters=(A B C D E)
-characters = $(shuf -e "${characters[@]}")
-echo "$characters"
+# The previous one did not save the shuffled characters into the array. This will save the randomly sorted array into
+# the characters array
 
+# Declaring an array
+declare -a characters
 
-#for i in 1 2 3 4 5 
-#do
-#	cat /dev/urandom| LC_CTYPE=C tr -dc 'A-F'|head -c 5
-#	echo
-#done
+# tr means transform.
+# AWK is the language
+# \t is tab
+# $0 is used to keep them as characters when passing
+# sort -n means sort while array is not NULL
+# cut -f2- prevents the random array being created into a file
+characters=($(echo A,B,C,D,E,F | tr ',' '\n' | awk 'BEGIN { srand() } { print rand() "\t" $0 }' | sort -n | cut -f2- ))
+
+echo "Order of routers being started"
+
+# Printing the entire array
+# Replace the @ with a number to print the character in that position
+echo ${characters[@]}
